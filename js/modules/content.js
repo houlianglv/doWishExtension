2f6c899e5aa795056164925c12c6e9e661cea77f
LvHouliang <houliang.lv@sap.com>
2015-03-16 18:27:01 +0800

    Houliang RequireJS

---
+++ js/modules/content.js
@@ -0,0 +1,68 @@
+requirejs.config(requirejsConfig);
+
+require(
+	['jquery'],
+	function($) {
+		var doWish = (function() {
+			var getTMallWish, getAmazonWish, getTaobaoWish;
+			getTMallWish = function() {
+				var jqueryMap = {
+					$price: $('.tm-price'),
+					$title: $('.tb-detail-hd h1'),
+					$img: $('.tb-thumb img')
+				};
+				if (jqueryMap.$price.size() > 0) {
+					var prices = [];
+					jqueryMap.$price.each(function(i) {
+						prices[i] = $(this).html();
+					});
+				}
+				var title = ""
+				jqueryMap.$title.size() > 0 ? title = jqueryMap.$title.html() : title;
+				if (jqueryMap.$img.size() > 0) {
+					var imgUrls = [];
+					jqueryMap.$img.each(function(i) {
+						imgUrls[i] = $(this).attr('src');
+					});
+				}
+				console.log(jqueryMap);
+				return {
+					prices: prices,
+					title: title,
+					imgUrls: imgUrls
+				};
+			};
+			getAmazonWish = function() {
+
+			};
+			getTaobaoWish = function() {
+
+			};
+			return {
+				getTMallWish: getTMallWish,
+				getAmazonWish: getAmazonWish,
+				getTaobaoWish: getTaobaoWish
+			};
+		})();
+
+		chrome.runtime.onConnect.addListener(function(port) {
+			console.assert(port.name == "doWish");
+			port.onMessage.addListener(function(command) {
+				if (command.name) {
+					var wish = doWish[command.name]();
+					// wish = {
+					// 	name: "basketball",
+					// 	price: "149",
+					// 	currency: "rmb",
+					// 	url: "detail.tmall.com/123144523"
+					// };
+					var wishJSON = JSON.stringify(wish); //circular structure cannot be convert!
+					console.log(wishJSON);
+					port.postMessage({
+						wish: wishJSON
+					});
+				}
+				console.log(command);
+			});
+		});
+	});
---
+++ js/requireConfig.js
@@ -0,0 +1,8 @@
+var requirejsConfig = {
+	//by default load any modules IDs from js/modules
+	baseUrl: '/js/modules',
+	//optionally specify different paths for specific modules
+	paths: {
+		lib: '/js/lib'
+	}
+};
